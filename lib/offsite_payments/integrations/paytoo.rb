5cf8341082573a7f2ea84c4a26294dce7c86fcf9
Ghias Arshad <ghias.arshad@arkhitech.com>
2017-07-24 16:56:06 +0500

    added www to urls

--- lib/offsite_payments/integrations/paytoo.rb
+++ lib/offsite_payments/integrations/paytoo.rb
@@ -172,9 +172,9 @@ module OffsitePayments #:nodoc:
               add_field('user[zipcode]', '10000')
               add_field('user[city]', 'Test')
               add_field('hash', "#{ENV['PAYTOO_HASH']}")
-              add_field('completed_url', 'https://mlmcustomer.org/paytoo/success')
-              add_field('cancelled_url', 'https://mlmcustomer.org/paytoo/cancel')
-              add_field('rejected_url', 'https://mlmcustomer.org/paytoo/cancel')
+              add_field('completed_url', 'https://www.mlmcustomer.org/paytoo/success')
+              add_field('cancelled_url', 'https://www.mlmcustomer.org/paytoo/cancel')
+              add_field('rejected_url', 'https://www.mlmcustomer.org/paytoo/cancel')
 
             end
 
